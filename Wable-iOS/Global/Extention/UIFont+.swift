fc8fb911b9506ddecc662a80fb2272a8d4c23d7f
binisnull <dbsqlssss0704@gmail.com>
2024-08-08 02:00:23 +0900

    [Setting] #1 - 디자인시스템 폰트 클래스변수로 변경

--- Wable-iOS/Global/Extention/UIFont+.swift
+++ Wable-iOS/Global/Extention/UIFont+.swift
@@ -7,33 +7,25 @@
 
 import UIKit
 
-enum FontName: String {
-    case head0, head1, head2
-    case body1, body2, body3, body4
-    case caption1, caption2, caption3, caption4
-
-    var rawValue: String {
-        switch self {
-        case .head0, .head1, .head2, .body1, .body3, .caption1, .caption3: return "Pretendard-SemiBold"
-        case .body2, .body4, .caption2, .caption4: return "Pretendard-Regular"
-        }
-    }
-
-    var size: CGFloat {
-        switch self {
-        case .head0: return 24.adjusted
-        case .head1: return 20.adjusted
-        case .head2: return 18.adjusted
-        case .body1, .body2: return 16.adjusted
-        case .body3, .body4: return 14.adjusted
-        case .caption1, .caption2: return 13.adjusted
-        case .caption3, .caption4: return 12.adjusted
-        }
-    }
+enum AppFontName: String {
+    case semiBoldFont = "Pretendard-SemiBold"
+    case regularFont = "Pretendard-Regular"
 }
 
 extension UIFont {
-    static func font(_ style: FontName) -> UIFont {
-        return UIFont(name: style.rawValue, size: style.size)!
-    }
+    
+    class var head0: UIFont { return UIFont(name: AppFontName.semiBoldFont.rawValue, size: 24.adjusted)! }
+    class var head1: UIFont { return UIFont(name: AppFontName.semiBoldFont.rawValue, size: 20.adjusted)! }
+    class var head2: UIFont { return UIFont(name: AppFontName.semiBoldFont.rawValue, size: 18.adjusted)! }
+
+    class var body1: UIFont { return UIFont(name: AppFontName.semiBoldFont.rawValue, size: 16.adjusted)! }
+    class var body2: UIFont { return UIFont(name: AppFontName.regularFont.rawValue, size: 16.adjusted)! }
+    class var body3: UIFont { return UIFont(name: AppFontName.semiBoldFont.rawValue, size: 14.adjusted)! }
+    class var body4: UIFont { return UIFont(name: AppFontName.regularFont.rawValue, size: 14.adjusted)! }
+
+    class var caption1: UIFont { return UIFont(name: AppFontName.semiBoldFont.rawValue, size: 13.adjusted)! }
+    class var caption2: UIFont { return UIFont(name: AppFontName.regularFont.rawValue, size: 13.adjusted)! }
+    class var caption3: UIFont { return UIFont(name: AppFontName.semiBoldFont.rawValue, size: 12.adjusted)! }
+    class var caption4: UIFont { return UIFont(name: AppFontName.regularFont.rawValue, size: 12.adjusted)! }
+    
 }
