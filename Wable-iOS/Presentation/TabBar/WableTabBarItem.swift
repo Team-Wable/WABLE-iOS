f1f6815225b26e89e4251fb7a8c7a7b3ae5e7138
binisnull <dbsqlssss0704@gmail.com>
2024-08-09 01:28:06 +0900

    [Setting] #1 - 로티 라이브러리 추가 및 탭바세팅

---
+++ Wable-iOS/Presentation/TabBar/WableTabBarItem.swift
@@ -0,0 +1,52 @@
+//
+//  WableTabBarItem.swift
+//  Wable-iOS
+//
+//  Created by 박윤빈 on 8/8/24.
+//
+
+import UIKit
+
+import SnapKit
+enum WableTabBarItem: CaseIterable {
+    case home
+    case info
+    case noti
+    case my
+    
+    var icon: UIImage {
+        switch self {
+        case .home: return ImageLiterals.Icon.icHomeDefault
+        case .info: return ImageLiterals.Icon.icInfoDefault
+        case .noti: return ImageLiterals.Icon.icNotiDefault
+        case .my: return ImageLiterals.Icon.icMyDefault
+        }
+    }
+    
+    var selectedIcon: UIImage {
+        switch self {
+        case .home: return ImageLiterals.Icon.icHomePress
+        case .info: return ImageLiterals.Icon.icInfoPress
+        case .noti: return ImageLiterals.Icon.icNotiPress
+        case .my: return ImageLiterals.Icon.icMyPress
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .home: return StringLiterals.TabBar.home
+        case .info: return StringLiterals.TabBar.info
+        case .noti: return StringLiterals.TabBar.noti
+        case .my: return StringLiterals.TabBar.my
+        }
+    }
+    
+    var targetViewController: UIViewController? {
+        switch self {
+        case .home: return HomeViewController()
+        case .info: return InfoViewController()
+        case .noti: return NotificationViewController()
+        case .my: return MyPageViewController()
+        }
+    }
+}
