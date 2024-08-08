bd3461162b684f45251cfd9e50ed05767a858bbb
binisnull <dbsqlssss0704@gmail.com>
2024-08-09 01:28:06 +0900

    [Setting] #1 - 로티 라이브러리 추가 및 탭바세팅

---
+++ Wable-iOS/Presentation/MyPage/ViewController/MyPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MyPageViewController.swift
+//  Wable-iOS
+//
+//  Created by 박윤빈 on 8/8/24.
+//
+
+import UIKit
+
+final class MyPageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .purple50
+    }
+}
