68e38403cf040cc79990613ccce2d3b2bd15da72
binisnull <dbsqlssss0704@gmail.com>
2024-08-20 20:51:37 +0900

    [Feat] #13 - 파일 복구

---
+++ Wable-iOS/Presentation/Write/ViewController/WriteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WriteViewController.swift
+//  Wable-iOS
+//
+//  Created by 박윤빈 on 8/20/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class WriteViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .purple50
+    }
+}
