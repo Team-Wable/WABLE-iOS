0c96214ed34777c89add7b8b70fddb15c076ea0d
Sangwoo Byeon <tkddn0518@naver.com>
2024-08-23 18:53:00 +0900

    [Feat] #21 - 글쓰기 뷰 MVVM 구조 생성

--- Wable-iOS/Presentation/Write/ViewController/WriteViewController.swift
+++ Wable-iOS/Presentation/Write/ViewController/WriteViewController.swift
@@ -6,12 +6,254 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
 import SnapKit
 
 final class WriteViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    static let showWriteToastNotification = Notification.Name("ShowWriteToastNotification")
+    
+    private var cancelBag = CancelBag()
+    private let viewModel: WriteViewModel
+    private var transparency: Int = 0
+    
+    private lazy var backButtonTapped = self.navigationBackButton.publisher(for: .touchUpInside).map { _ in }.eraseToAnyPublisher()
+    private lazy var postButtonTapped = self.rootView.writeTextView.postButton.publisher(for: .touchUpInside).map { _ in }.eraseToAnyPublisher()
+    
+    // MARK: - UI Components
+    
+    private var navigationBackButton = BackButton()
+    private let rootView = WriteView()
+//    private let banView = WriteBanView()
+    
+    // MARK: - Life Cycles
+    
+    init(viewModel: WriteViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        view = rootView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .purple50
+        
+        setDelegate()
+        setAddTarget()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tabBarController?.tabBar.isHidden = true
+        self.tabBarController?.tabBar.isTranslucent = true
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        getAPI()
+        bindViewModel()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.tabBarController?.tabBar.isHidden = false
+        self.tabBarController?.tabBar.isTranslucent = false
+        
+        NotificationCenter.default.removeObserver(self, name: UITextView.textDidChangeNotification, object: nil)
+
+    }
+}
+
+// MARK: - Extensions
+
+extension WriteViewController {
+    private func setUI() {
+        self.view.backgroundColor = .wableWhite
+        self.navigationController?.navigationBar.isHidden = false
+        self.navigationItem.hidesBackButton = true
+    }
+    
+    private func setHierarchy() {
+        self.navigationController?.navigationBar.addSubviews(navigationBackButton)
+    }
+    
+    private func setLayout() {
+        navigationBackButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(12.adjusted)
+        }
+    }
+    
+    private func setDelegate() {
+//        self.rootView.writeCanclePopupView.delegate = self
+    }
+    
+    private func setAddTarget() {
+        self.rootView.writeTextView.photoButton.addTarget(self, action: #selector(photoButtonTapped), for: .touchUpInside)
+    }
+    
+    private func bindViewModel() {
+        let input = WriteViewModel.Input(backButtonTapped: backButtonTapped,
+                                         postButtonTapped: postButtonTapped)
+        
+        let output = self.viewModel.transform(from: input, cancelBag: self.cancelBag)
+        output.pushOrPopViewController
+            .receive(on: RunLoop.main)
+            .sink { value in
+                if value == 0 {
+                    self.navigationController?.popViewController(animated: true)
+                } else {
+                    self.WriteCompleted()
+                    self.navigationController?.popViewController(animated: true)
+                }
+            }
+            .store(in: self.cancelBag)
+    }
+    
+    private func WriteCompleted() {
+        NotificationCenter.default.post(name: WriteViewController.showWriteToastNotification, object: nil, userInfo: ["showToast": true])
+    }
+    
+    private func popupNavigation() {
+//        self.tabBarController?.selectedIndex = 0
+//        if let selectedViewController = self.tabBarController?.selectedViewController {
+//            self.applyTabBarAttributes(to: selectedViewController.tabBarItem, isSelected: true)
+//        }
+//        let myViewController = self.tabBarController?.viewControllers ?? [UIViewController()]
+//        for (index, controller) in myViewController.enumerated() {
+//            if let tabBarItem = controller.tabBarItem {
+//                if index != self.tabBarController?.selectedIndex {
+//                    self.applyTabBarAttributes(to: tabBarItem, isSelected: false)
+//                }
+//            }
+//        }
+        self.navigationController?.popViewController(animated: false)
+    }
+    
+    @objc private func photoButtonTapped() {
+        let status = PHPhotoLibrary.authorizationStatus(for: .addOnly)
+        
+        switch status {
+        case .authorized, .limited:
+            presentPicker()
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization(for: .addOnly) { [weak self] status in
+                DispatchQueue.main.async {
+                    if status == .authorized {
+                        self?.presentPicker()
+                    }
+                }
+            }
+        case .denied, .restricted:
+            authSettingOpen()
+        default:
+            break
+        }
+    }
+    
+    private func presentPicker() {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images // 이미지만 필터링
+        configuration.selectionLimit = 1 // 선택 제한
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    private func authSettingOpen() {
+        let message = StringLiterals.Camera.photoNoAuth
+        
+        let alert = UIAlertController(title: "설정", message: message, preferredStyle: .alert)
+        
+        let cancle = UIAlertAction(title: "닫기", style: .default)
+        
+        let confirm = UIAlertAction(title: "권한설정하기", style: .default) { (UIAlertAction) in
+            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+        }
+        
+        alert.addAction(cancle)
+        alert.addAction(confirm)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+//    @objc
+//    private func cancleNavigationBarButtonTapped() {
+//        if self.rootView.writeTextView.contentTextView.text == "" && self.rootView.writeTextView.linkTextView.text == "" && self.rootView.writeTextView.photoImageView.image == nil {
+//            popupNavigation()
+//        } else {
+//            self.rootView.writeCanclePopupView.alpha = 1
+//        }
+//    }
+//    
+//    @objc
+//    private func promiseButtonTapped() {
+//        self.banView.removeFromSuperview()
+//        self.navigationController?.popViewController(animated: true)
+//    }
+}
+
+// MARK: - Network
+
+extension WriteViewController {
+    private func getAPI() {
+//        if UserDefaults.standard.integer(forKey: "memberGhost") <= -85 {
+//            self.banView.snp.makeConstraints {
+//                $0.edges.equalToSuperview()
+//            }
+//        } else {
+//            self.banView.removeFromSuperview()
+//        }
+    }
+}
+
+//extension WriteViewController: DontBePopupDelegate {
+//    func cancleButtonTapped() {
+//        self.rootView.writeCanclePopupView.alpha = 0
+//    }
+//    
+//    func confirmButtonTapped() {
+//        self.rootView.writeCanclePopupView.alpha = 0
+//        popupNavigation()
+//    }
+//}
+
+extension WriteViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true)
+        
+        guard let selectedImage = results.first else { return }
+        
+        selectedImage.itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
+            DispatchQueue.main.async {
+                if let image = image as? UIImage {
+                    self.rootView.writeTextView.photoImageView.isHidden = false
+                    self.rootView.writeTextView.photoImageView.image = image
+                    
+                    self.rootView.writeTextView.contentTextView.snp.remakeConstraints {
+                        $0.top.equalTo(self.rootView.writeTextView.photoImageView.snp.bottom).offset(12.adjusted)
+                        $0.leading.trailing.equalToSuperview().inset(24.adjusted)
+                        $0.bottom.equalTo(self.rootView.writeTextView.keyboardToolbarView.snp.top)
+                    }
+                } else if let error = error {
+                    print(error)
+                }
+            }
+        }
     }
 }
---
+++ Wable-iOS/Presentation/Write/ViewModel/WriteViewModel.swift
@@ -0,0 +1,130 @@
+//
+//  WriteViewModel.swift
+//  Wable-iOS
+//
+//  Created by 변상우 on 8/23/24.
+//
+
+import Combine
+import Foundation
+import UIKit
+
+final class WriteViewModel: ViewModelType {
+    
+    private let cancelBag = CancelBag()
+//    private let networkProvider: NetworkServiceType
+    
+    private let pushOrPopViewController = PassthroughSubject<Int, Never>()
+    
+    struct Input {
+        let backButtonTapped: AnyPublisher<Void, Never>
+        let postButtonTapped: AnyPublisher<Void, Never>
+    }
+    
+    struct Output {
+        let pushOrPopViewController: PassthroughSubject<Int, Never>
+    }
+    
+    func transform(from input: Input, cancelBag: CancelBag) -> Output {
+        input.backButtonTapped
+            .sink { _ in
+                self.pushOrPopViewController.send(0)
+            }
+            .store(in: cancelBag)
+        
+        input.postButtonTapped
+            .sink { _ in
+                self.pushOrPopViewController.send(1)
+            }
+            .store(in: cancelBag)
+        
+        return Output(pushOrPopViewController: pushOrPopViewController)
+    }
+    
+//    init(networkProvider: NetworkServiceType) {
+//        self.networkProvider = networkProvider
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+}
+
+extension WriteViewModel {
+//    private func postWriteContentAPI(accessToken: String, contentText: String) async throws -> BaseResponse<EmptyResponse>? {
+//        do {
+//            let result: BaseResponse<EmptyResponse>? = try await
+//            self.networkProvider.donNetwork(
+//                type: .post,
+//                baseURL: Config.baseURL + "/content",
+//                accessToken: accessToken,
+//                body: WriteContentRequestDTO(contentText: contentText),
+//                pathVariables: ["":""]
+//            )
+//            return result
+//        } catch {
+//            return nil
+//        }
+//    }
+//    
+//    private func postWriteContentAPI(contentText: String, photoImage: UIImage?) {
+//        guard let url = URL(string: Config.baseURL.dropLast() + "2/content") else { return }
+//        guard let accessToken = KeychainWrapper.loadToken(forKey: "accessToken") else { return }
+//        
+//        let parameters: [String: Any] = [
+//            "contentText": contentText
+//        ]
+//        
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//        
+//        // Multipart form data 생성
+//        let boundary = "Boundary-\(UUID().uuidString)"
+//        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+//        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+//        
+//        var requestBodyData = Data()
+//        
+//        // 게시글 본문 데이터 추가
+//        requestBodyData.append("--\(boundary)\r\n".data(using: .utf8)!)
+//        requestBodyData.append("Content-Disposition: form-data; name=\"text\"\r\n\r\n".data(using: .utf8)!)
+//        requestBodyData.append(try! JSONSerialization.data(withJSONObject: parameters, options: []))
+//        requestBodyData.append("\r\n".data(using: .utf8)!)
+//        
+//        if let image = photoImage {
+//            let imageData = image.jpegData(compressionQuality: 0.1)!
+//            
+//            // 게시글 이미지 데이터 추가
+//            requestBodyData.append("--\(boundary)\r\n".data(using: .utf8)!)
+//            requestBodyData.append("Content-Disposition: form-data; name=\"image\"; filename=\"dontbe.jpeg\"\r\n".data(using: .utf8)!)
+//            requestBodyData.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
+//            requestBodyData.append(imageData)
+//            requestBodyData.append("\r\n".data(using: .utf8)!)
+//        }
+//        
+//        requestBodyData.append("--\(boundary)--\r\n".data(using: .utf8)!)
+//        
+//        // HTTP body에 데이터 설정
+//        request.httpBody = requestBodyData
+//        
+//        // URLSession으로 요청 보내기
+//        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+//            if let error = error {
+//                print("Error:", error)
+//                return
+//            }
+//            
+//            // 응답 처리
+//            if let response = response as? HTTPURLResponse {
+//                print(response)
+//                print("Response status code:", response.statusCode)
+//            }
+//            
+//            if let data = data {
+//                // 서버 응답 데이터 처리
+//                print("Response data:", String(data: data, encoding: .utf8) ?? "Empty response")
+//            }
+//        }
+//        task.resume()
+//    }
+}
