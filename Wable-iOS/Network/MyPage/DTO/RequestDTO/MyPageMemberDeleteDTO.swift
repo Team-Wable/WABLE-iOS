a60f701616c75c6375bb5cea5a742ecaf18e6207
Sangwoo Byeon <tkddn0518@naver.com>
2024-08-24 12:22:36 +0900

    [Feat] #23 - 서버 통신을 위한 DTO 생성

---
+++ Wable-iOS/Network/MyPage/DTO/RequestDTO/MyPageMemberDeleteDTO.swift
@@ -0,0 +1,14 @@
+//
+//  MyPageMemberDeleteDTO.swift
+//  Wable-iOS
+//
+//  Created by 변상우 on 8/24/24.
+//
+
+import Foundation
+
+// MARK: - MyPageMemberDeleteDTO
+
+struct MyPageMemberDeleteDTO: Encodable {
+    let deleted_reason: String
+}
