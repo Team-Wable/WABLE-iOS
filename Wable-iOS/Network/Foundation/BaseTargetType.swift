a709da19853da5033346afd7bc33eb77422022a5
binisnull <dbsqlssss0704@gmail.com>
2024-08-24 13:51:13 +0900

    [Fix] #19 - Moya 네트워크 Foundation 수정사항

--- Wable-iOS/Network/Foundation/BaseTargetType.swift
+++ Wable-iOS/Network/Foundation/BaseTargetType.swift
@@ -13,9 +13,8 @@ protocol BaseTargetType: TargetType{ }
 extension BaseTargetType{
 
     var baseURL: URL {
-        // config 파일 생성 후 주석 해제
-//        let baseURL = Bundle.main.infoDictionary?["BASE_URL"] as? String ?? ""
-        let baseURL = "https://sample.base.url"
+
+        let baseURL = Config.baseURL
         return URL(string: baseURL)!
     }
 
