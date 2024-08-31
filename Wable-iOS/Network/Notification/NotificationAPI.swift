864c69e8aa864dd91161722a2fdb265e23f33be7
binisnull <dbsqlssss0704@gmail.com>
2024-08-31 19:03:16 +0900

    [Network] #27 - Noti API 연동 구현 완료

---
+++ Wable-iOS/Network/Notification/NotificationAPI.swift
@@ -0,0 +1,36 @@
+//
+//  NotificationAPI.swift
+//  Wable-iOS
+//
+//  Created by 박윤빈 on 8/31/24.
+//
+
+import Foundation
+
+import Moya
+
+final class NotificationAPI: BaseAPI {
+    static let shared =  NotificationAPI()
+        private var notiProvider = MoyaProvider<NotificationRouter>(plugins: [MoyaLoggingPlugin()])
+        private override init() {}
+}
+
+extension NotificationAPI {
+    func getNotiInfo(cursor: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+        notiProvider.request(.getNotiInfo(param: cursor)) { result in
+            self.disposeNetwork(result,
+                                dataModel: [InfoNotificationDTO].self,
+                                completion: completion)
+            
+        }
+    }
+    
+    func getNotiActivity(cursor: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+        notiProvider.request(.getNotiActivity(param: cursor)) { result in
+            self.disposeNetwork(result,
+                                dataModel: [ActivityNotificationDTO].self,
+                                completion: completion)
+            
+        }
+    }
+}
