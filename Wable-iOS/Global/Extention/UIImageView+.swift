41dcffe264f42ab37c4a236c391fb9165f8d5bc3
Sangwoo Byeon <tkddn0518@naver.com>
2024-08-31 13:49:41 +0900

    [Feat] #26 - 기본 이미지로 인한 KingFisher 오류 해결

--- Wable-iOS/Global/Extention/UIImageView+.swift
+++ Wable-iOS/Global/Extention/UIImageView+.swift
@@ -10,35 +10,51 @@ import UIKit
 import Kingfisher
 
 extension UIImageView {
+    
+    // 기본 이미지 딕셔너리 고정
+    private var defaultImages: [UIImage : String] {
+        return [
+            ImageLiterals.Image.imgProfile1 : "PURPLE",
+            ImageLiterals.Image.imgProfile2 : "BLUE",
+            ImageLiterals.Image.imgProfile3 : "GREEN"
+        ]
+    }
+    
     func load(url: String) {
-        guard let url = URL(string: url) else { return }
-        
-        self.kf.indicatorType = .activity
-        self.kf.setImage(
-            with: url,
-            placeholder: nil,
-            options: [
-                .scaleFactor(UIScreen.main.scale),
-                .cacheOriginalImage
-            ]
-        ) { result in
-            switch result {
-            case .success(let value):
-                DispatchQueue.main.async { [weak self] in
-                    self?.image = value.image
-                    self?.layer.cornerRadius = (self?.frame.size.width ?? 0) / 2.adjusted
-                    self?.contentMode = .scaleAspectFill
-                    self?.clipsToBounds = true
+        // "PURPLE", "BLUE", "GREEN"과 같은 값을 처리
+        if let defaultImage = defaultImages.first(where: { $0.value == url })?.key {
+            // 기본 이미지 적용
+            self.image = defaultImage
+            self.layer.cornerRadius = self.frame.size.width / 2.adjusted
+            self.contentMode = .scaleAspectFill
+            self.clipsToBounds = true
+        } else if let imageURL = URL(string: url) {
+            self.kf.indicatorType = .activity
+            self.kf.setImage(
+                with: imageURL,
+                placeholder: nil,
+                options: [
+                    .scaleFactor(UIScreen.main.scale),
+                    .cacheOriginalImage
+                ]) { result in
+                    switch result {
+                    case .success(let value):
+                        DispatchQueue.main.async { [weak self] in
+                            self?.image = value.image
+                            self?.layer.cornerRadius = (self?.frame.size.width ?? 0) / 2.adjusted
+                            self?.contentMode = .scaleAspectFill
+                            self?.clipsToBounds = true
+                        }
+                    case .failure(let error):
+                        print("Error loading image: \(error)")
+                        
+                        DispatchQueue.main.async { [weak self] in
+                            self?.layer.cornerRadius = (self?.frame.size.width ?? 0) / 2.adjusted
+                            self?.contentMode = .scaleAspectFill
+                            self?.clipsToBounds = true
+                        }
+                    }
                 }
-            case .failure(let error):
-                print("Error loading image: \(error)")
-                
-                DispatchQueue.main.async { [weak self] in
-                    self?.layer.cornerRadius = (self?.frame.size.width ?? 0) / 2.adjusted
-                    self?.contentMode = .scaleAspectFill
-                    self?.clipsToBounds = true
-                }
-            }
         }
     }
     
@@ -57,14 +73,14 @@ extension UIImageView {
             case .success(let value):
                 DispatchQueue.main.async {
                     self.image = value.image
-                    self.layer.cornerRadius = 4.adjusted
+                    self.layer.cornerRadius = 8.adjusted
                     self.contentMode = .scaleAspectFill
                     self.clipsToBounds = true
                 }
             case .failure(let error):
                 print("Error loading image: \(error)")
                 
-                self.layer.cornerRadius = 4.adjusted
+                self.layer.cornerRadius = 8.adjusted
                 self.contentMode = .scaleAspectFill
                 self.clipsToBounds = true
             }
