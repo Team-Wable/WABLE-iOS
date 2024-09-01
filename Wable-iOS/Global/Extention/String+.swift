4828f825aff373fec58e1e6fa0381801cad2d520
Sangwoo Byeon <tkddn0518@naver.com>
2024-09-01 12:56:06 +0900

    [Feat] #26 - 필요한 기능, String Extension 함수 추가

--- Wable-iOS/Global/Extention/String+.swift
+++ Wable-iOS/Global/Extention/String+.swift
@@ -16,4 +16,32 @@ extension String {
         let consonantScalarRange: ClosedRange<UInt32> = 12593...12622
         return consonantScalarRange ~= scalar
     }
+    
+    func formattedTime() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        if let postDate = dateFormatter.date(from: self) {
+            let components = Calendar.current.dateComponents([.year, .month, .weekOfYear, .day, .hour, .minute], from: postDate, to: Date())
+            if let year = components.year, year > 0 {
+                return "\(year)년 전"
+            } else if let month = components.month, month > 0 {
+                return "\(month)개월 전"
+            } else if let week = components.weekOfYear, week > 0 {
+                return "\(week)주 전"
+            } else if let day = components.day, day > 0 {
+                return "\(day)일 전"
+            } else if let hour = components.hour, hour > 0 {
+                return "\(hour)시간 전"
+            } else if let minute = components.minute, minute >= 1 {
+                return "\(minute)분 전"
+            } else if let minute = components.minute, minute == 0 {
+                return "지금"
+            } else {
+                return "알 수 없음"
+            }
+        } else {
+            return "알 수 없음"
+        }
+    }
 }
