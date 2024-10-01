f53574aa83d21c5fe1146da8cd0a04d5fc0f3a99
binisnull <dbsqlssss0704@gmail.com>
2024-10-01 19:57:57 +0900

    [Fix] #46 - AmplitudeManager 싱글톤 패턴으로 생성

--- Wable-iOS/Global/Shared/AmplitudeManager.swift
+++ Wable-iOS/Global/Shared/AmplitudeManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+import AmplitudeSwift
+
+class AmplitudeManager {
+    
+    static let shared =  AmplitudeManager()
+    
+    let amplitude: Amplitude
+
+    private init() {
+        amplitude = Amplitude(configuration: Configuration(
+            apiKey: Config.amplitudeAppKey
+                ))
+        
+    }
+    
+    func trackEvent(tag: String) {
+        amplitude.track(eventType: tag)
+    }
+    
+}
