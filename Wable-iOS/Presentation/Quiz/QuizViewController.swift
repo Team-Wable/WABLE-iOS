192326824ffdd3175c62c1f96e0f2c11fc90fb3b
youz2me <kynhun20@gachon.ac.kr>
2024-10-18 14:30:00 +0900

    [Add] #294 - 퀴즈 화면 ViewController 파일 추가

---
+++ Wable-iOS/Presentation/Quiz/QuizViewController.swift
@@ -0,0 +1,15 @@
+//
+//  QuizViewController.swift
+//  Wable-iOS
+//
+//  Created by Youjin Lee on 10/21/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+public final class QuizViewController: UIViewController {
+    
+}
