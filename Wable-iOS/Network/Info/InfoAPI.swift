d21602d8fe1a3dd446d7f45f38fa13f90b8d44a1
JinUng41 <ingjwjw@naver.com>
2024-11-15 06:05:52 +0900

    [Fix] #60 - 추가된 네트워크 통신 방법을 활용하여 데이터 통신
    - 네트워크 사용 예시 격입니다.

--- Wable-iOS/Network/Info/InfoAPI.swift
+++ Wable-iOS/Network/Info/InfoAPI.swift
@@ -6,22 +6,24 @@
 //
 
 import Foundation
+import Combine
 
+import CombineMoya
 import Moya
 
 final class InfoAPI: BaseAPI {
     static let shared =  InfoAPI()
-        private var infoProvider = MoyaProvider<InfoRouter>(plugins: [MoyaLoggingPlugin()])
-        private override init() {}
+    private var infoProvider = MoyaProvider<InfoRouter>(plugins: [MoyaLoggingPlugin()])
+    private override init() {}
 }
 
 extension InfoAPI {
-    func getMatchInfo(completion: @escaping (NetworkResult<Any>) -> Void) {
-        infoProvider.request(.getMatchInfo) { result in
-            self.disposeNetwork(result,
-                                dataModel: [TodayMatchesDTO].self,
-                                completion: completion)
-            
-        }
+    func getMatchInfo() -> AnyPublisher<[TodayMatchesDTO]?, WableNetworkError> {
+        infoProvider.requestPublisher(.getMatchInfo)
+            .tryMap { [weak self] response -> [TodayMatchesDTO]? in
+                return try self?.parseResponse(statusCode: response.statusCode, data: response.data)
+            }
+            .mapError { $0 as? WableNetworkError ?? .unknownError($0.localizedDescription) }
+            .eraseToAnyPublisher()
     }
 }
