aff4076a60b79d028b568054e48c1553836a628b
JinUng41 <ingjwjw@naver.com>
2024-11-24 06:31:37 +0900

    [Fix] #65 - 버튼을 제외한 모든 요소 스크롤 되도록 수정

--- Wable-iOS/Presentation/Info/Ranking/View/Cell/RankCell.swift
+++ Wable-iOS/Presentation/Info/Ranking/View/Cell/RankCell.swift
@@ -71,6 +71,7 @@ final class RankCell: UICollectionViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         
