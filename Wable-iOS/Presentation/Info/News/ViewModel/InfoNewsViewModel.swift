e04618d92ca5ce9332b0cfeaacdc55ce2df57020
JinUng41 <ingjwjw@naver.com>
2024-11-25 01:14:47 +0900

    [Feat] #67 - 뉴스 디테일 뷰 구현 및 사진 팝업 구현

--- Wable-iOS/Presentation/Info/News/ViewModel/InfoNewsViewModel.swift
+++ Wable-iOS/Presentation/Info/News/ViewModel/InfoNewsViewModel.swift
@@ -21,31 +21,62 @@ final class InfoNewsViewModel {
 extension InfoNewsViewModel: ViewModelType {
     struct Input {
         let viewWillAppear: AnyPublisher<Void, Never>
+        let collectionViewDidRefresh: AnyPublisher<Void, Never>
+        let collectionViewDidSelect: AnyPublisher<Int, Never>
+        let collectionViewDidEndDrag: AnyPublisher<Void, Never>
     }
     
     struct Output {
         let news: AnyPublisher<[NewsDTO], Never>
+        let navigateToDetail: AnyPublisher<NewsDTO, Never>
     }
     
     func transform(from input: Input, cancelBag: CancelBag) -> Output {
-        let news = input.viewWillAppear
+        let news = CurrentValueSubject<[NewsDTO], Never>([])
+        
+        input.viewWillAppear
             .flatMap { [weak self] _ -> AnyPublisher<[NewsDTO], Never> in
                 guard let self else {
                     return Just([]).eraseToAnyPublisher()
                 }
                 
                 return service.getNews(cursor: -1)
-                    .compactMap { $0 }
                     .mapWableNetworkError()
                     .replaceError(with: [])
+                    .compactMap { $0 }
                     .eraseToAnyPublisher()
             }
-            .removeDuplicates()
             .map { NewsTimeFormatter(news: $0).formattedNews() }
+            .subscribe(news)
+            .store(in: cancelBag)
+        
+        input.collectionViewDidRefresh
+            .handleEvents(receiveRequest: { [weak self] _ in
+                self?.cursor = -1
+            })
+            .flatMap { [weak self] _ -> AnyPublisher<[NewsDTO], Never> in
+                guard let self else {
+                    return Just([]).eraseToAnyPublisher()
+                }
+                
+                return service.getNews(cursor: -1)
+                    .mapWableNetworkError()
+                    .replaceError(with: [])
+                    .compactMap { $0 }
+                    .eraseToAnyPublisher()
+            }
+            .map { NewsTimeFormatter(news: $0).formattedNews() }
+            .subscribe(news)
+            .store(in: cancelBag)
+        
+        let navigateToDetail = input.collectionViewDidSelect
+            .filter { $0 < news.value.count }
+            .map { news.value[$0] }
             .eraseToAnyPublisher()
         
         return Output(
-            news: news
+            news: news.eraseToAnyPublisher(),
+            navigateToDetail: navigateToDetail
         )
     }
 }
