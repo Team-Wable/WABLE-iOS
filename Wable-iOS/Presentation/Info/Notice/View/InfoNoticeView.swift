42ef33e5aca6205e159e0668c4147a0ac7da8708
JinUng41 <ingjwjw@naver.com>
2024-11-26 02:21:53 +0900

    [Add] #73 - 구현해야 할 객체 파일과 DTO 파일 추가

---
+++ Wable-iOS/Presentation/Info/Notice/View/InfoNoticeView.swift
@@ -0,0 +1,8 @@
+//
+//  InfoNoticeView.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 11/26/24.
+//
+
+import Foundation
---
+++ Wable-iOS/Presentation/Info/Notice/ViewController/InfoNoticeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  InfoNoticeViewController.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 11/26/24.
+//
+
+import Foundation
