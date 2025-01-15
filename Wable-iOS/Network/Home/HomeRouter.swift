f5bcee1098318fcf7c8d10581042bf4f4513d26a
binisnull <dbsqlssss0704@gmail.com>
2025-01-15 18:58:53 +0900

    [Fix] #87 - HomeAPI 구현

--- Wable-iOS/Network/Home/HomeRouter.swift
+++ Wable-iOS/Network/Home/HomeRouter.swift
@@ -14,6 +14,11 @@ enum HomeRouter {
     case patchFCMToken(param: UserProfileRequestDTO)
     case postReply(param: Int, requestBody: WriteReplyRequestV3DTO)
     case postBan(requestBody: BanRequestDTO)
+    case postFeedLike(contentID: Int)
+    case deleteFeedLike(contentID: Int)
+    case deleteFeed(contentID: Int)
+    case postBeGhost(param: PostTransparencyRequestDTO)
+    case postReport(param: ReportRequestDTO)
 }
 
 extension HomeRouter: BaseTargetType {
@@ -27,6 +32,16 @@ extension HomeRouter: BaseTargetType {
             return StringLiterals.Endpoint.Home.postReply(contentID: contentID)
         case .postBan:
             return StringLiterals.Endpoint.Home.postBan
+        case .postFeedLike(let contentID):
+            return StringLiterals.Endpoint.Home.postFeedLike(contentID: contentID)
+        case .deleteFeedLike(let contentID):
+            return StringLiterals.Endpoint.Home.deleteFeedLike(contentID: contentID)
+        case .deleteFeed(let contentID):
+            return StringLiterals.Endpoint.Home.deleteFeed(contentID: contentID)
+        case .postBeGhost(let param):
+            return StringLiterals.Endpoint.Home.postOpacityDown
+        case .postReport:
+            return StringLiterals.Endpoint.Home.postReport
         }
     }
     
@@ -36,10 +51,10 @@ extension HomeRouter: BaseTargetType {
             return .get
         case .patchFCMToken:
             return .patch
-        case .postReply:
+        case .postReply, .postBan, .postFeedLike, .postBeGhost, .postReport:
             return .post
-        case .postBan:
-            return .post
+        case .deleteFeed, .deleteFeedLike:
+            return .delete
         }
     }
     
@@ -47,8 +62,8 @@ extension HomeRouter: BaseTargetType {
         switch self {
         case .getContent(let cursor):
             return .requestParameters(parameters: ["cursor": cursor], encoding: URLEncoding.queryString)
+            
         case .patchFCMToken(let data):
-
             var formData = [MultipartFormData]()
             
             // fcmToken 추가
@@ -61,7 +76,6 @@ extension HomeRouter: BaseTargetType {
             let pushAlarmData = String(describing: data.isPushAlarmAllowed).data(using: .utf8) ?? Data()
             let pushAlarmPart = MultipartFormData(provider: .data(pushAlarmData), name: "isPushAlarmAllowed")
             formData.append(pushAlarmPart)
-            
             return .uploadMultipart(formData)
             
         case .postReply(_, let requestBody):
@@ -69,12 +83,25 @@ extension HomeRouter: BaseTargetType {
 
         case .postBan(let requestBody):
             return .requestJSONEncodable(requestBody)
+            
+        case .postFeedLike:
+            let requestBody = ContentLikeRequestDTO(alarmTriggerType: "contentLiked")
+            return .requestJSONEncodable(requestBody)
+            
+        case .deleteFeedLike, .deleteFeed:
+            return .requestPlain
+            
+        case .postBeGhost(let requestBody):
+            return .requestJSONEncodable(requestBody)
+            
+        case .postReport(let requestBody):
+            return .requestJSONEncodable(requestBody)
         }
     }
     
     var headers: [String : String]? {
         switch self {
-        case .getContent, .postReply, .postBan:
+        case .getContent, .postReply, .postBan, .postFeedLike, .deleteFeedLike, .postBeGhost, .deleteFeed, .postReport:
             return APIConstants.hasTokenHeader
         case .patchFCMToken:
             return APIConstants.multipartHeader
