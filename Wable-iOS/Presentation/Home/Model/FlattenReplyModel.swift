7b5c22909978a58c88a774e0b15276a0518d491e
binisnull <dbsqlssss0704@gmail.com>
2025-01-26 02:29:00 +0900

    [Fix] #90 - 상세보기에 필요한 API 연결

--- Wable-iOS/Presentation/Home/Model/FlattenReplyModel.swift
+++ Wable-iOS/Presentation/Home/Model/FlattenReplyModel.swift
@@ -23,6 +23,65 @@ struct FlattenReplyModel {
 
 extension FlattenReplyModel: Hashable {
     static func == (lhs: FlattenReplyModel, rhs: FlattenReplyModel) -> Bool {
-        lhs.commentID == rhs.commentID
+        return lhs.commentID == rhs.commentID &&
+               lhs.memberID == rhs.memberID &&
+               lhs.memberProfileURL == rhs.memberProfileURL &&
+               lhs.memberNickname == rhs.memberNickname &&
+               lhs.isGhost == rhs.isGhost &&
+               lhs.memberGhost == rhs.memberGhost &&
+               lhs.isLiked == rhs.isLiked &&
+               lhs.commentLikedNumber == rhs.commentLikedNumber &&
+               lhs.commentText == rhs.commentText &&
+               lhs.time == rhs.time &&
+               lhs.isDeleted == rhs.isDeleted &&
+               lhs.memberFanTeam == rhs.memberFanTeam &&
+               lhs.parentCommentID == rhs.parentCommentID &&
+               lhs.isBlind == rhs.isBlind
+    }
+
+}
+
+extension FlattenReplyModel {
+    func editWith(
+        isGhost: Bool? = nil,
+        memberGhost: Int? = nil,
+        isLiked: Bool? = nil,
+        commentLikedNumber: Int? = nil,
+        isBlind: Bool? = nil
+    ) -> FlattenReplyModel {
+        return FlattenReplyModel(
+            commentID: commentID,
+            memberID: memberID,
+            memberProfileURL: memberProfileURL,
+            memberNickname: memberNickname,
+            isGhost: isGhost ?? self.isGhost,
+            memberGhost: memberGhost ?? self.memberGhost,
+            isLiked: isLiked ?? self.isLiked,
+            commentLikedNumber: commentLikedNumber ?? self.commentLikedNumber,
+            commentText: commentText,
+            time: time,
+            isDeleted: isDeleted,
+            memberFanTeam: memberFanTeam,
+            parentCommentID: parentCommentID,
+            isBlind: isBlind ?? self.isBlind
+        )
+    }
+    
+    func defaultValue() -> FlattenReplyModel {
+        return FlattenReplyModel(
+            commentID: -1,
+            memberID: -1,
+            memberProfileURL: "",
+            memberNickname: "",
+            isGhost: false,
+            memberGhost: -1,
+            isLiked: false,
+            commentLikedNumber: -1,
+            commentText: "",
+            time: "",
+            isDeleted: false,
+            memberFanTeam: "",
+            parentCommentID: -1,
+            isBlind: false)
     }
 }
