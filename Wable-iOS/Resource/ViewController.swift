9649c58507b696baad8e8365a50ff16e9e1953b5
youz2me <kynhun20@gachon.ac.kr>
2025-03-02 01:34:21 +0900

    [Delete] #104 - 테스트 코드 삭제

--- Wable-iOS/Resource/ViewController.swift
+++ Wable-iOS/Resource/ViewController.swift
@@ -11,15 +11,7 @@ import Then
 import SnapKit
 
 class ViewController: UIViewController {
-    private let testLabel = UILabel().then {
-        $0.text = "클린 LCK 팬 커뮤니티\n와블에서 함께 해요"
-        $0.setLabel(.head0)
-        $0.textColor = .wableBlack
-        $0.numberOfLines = 2
-        $0.textAlignment = .center
-        
-    }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,16 +19,8 @@ class ViewController: UIViewController {
     }
     
     // MARK: - Setup Method
-
+    
     private func setupView() {
         view.backgroundColor = .white
-        
-        view.addSubview(testLabel)
-        
-        testLabel.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalToSuperview().offset(148)
-        }
     }
 }
-
