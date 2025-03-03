51a8a37ffb6e7c2008c5142a8ef76167e60b5072
JinUng41 <ingjwjw@naver.com>
2025-03-04 03:02:05 +0900

    [Feat] #110 - 텍스트필드 익스텐션 메서드 추가
    - 왼쪽, 오른쪽 패딩값을 추가할 수 있는 메서드 구현

--- Wable-iOS/Presentation/UIHelper/UITextField+.swift
+++ Wable-iOS/Presentation/UIHelper/UITextField+.swift
@@ -35,4 +35,29 @@ extension UITextField {
         
         self.defaultTextAttributes = attributes
     }
+    
+    // MARK: - addPadding
+
+    /// `UITextField`의 왼쪽 및 오른쪽에 패딩을 추가합니다.
+    ///
+    /// - Parameters:
+    ///   - left: 왼쪽 패딩 값 (`CGFloat`). 기본값은 `nil`
+    ///   - right: 오른쪽 패딩 값 (`CGFloat`). 기본값은 `nil`
+    ///
+    /// 사용 예시:
+    /// ```swift
+    /// let textField = UITextField()
+    /// textField.addPadding(left: 10, right: 10)
+    /// ```
+    func addPadding(left: CGFloat? = nil, right: CGFloat? = nil) {
+        if let left {
+            leftView = UIView(frame: CGRect(x: 0, y: 0, width: left, height: 0))
+            leftViewMode = .always
+        }
+        
+        if let right {
+            rightView = UIView(frame: CGRect(x: 0, y: 0, width: right, height: 0))
+            rightViewMode = .always
+        }
+    }
 }
