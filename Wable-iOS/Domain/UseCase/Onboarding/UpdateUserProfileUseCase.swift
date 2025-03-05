25674c099f0e955dccd98ae153f89abb93dcd45d
youz2me <kynhun20@gachon.ac.kr>
2025-03-06 05:58:27 +0900

    [Feat] #113 - 온보딩 UseCase 구현

---
+++ Wable-iOS/Domain/UseCase/Onboarding/UpdateUserProfileUseCase.swift
@@ -0,0 +1,24 @@
+//
+//  UpdateUserProfileUseCase.swift
+//  Wable-iOS
+//
+//  Created by YOUJIM on 3/6/25.
+//
+
+
+import Combine
+
+final class UpdateUserProfileUseCase {
+    let repository: ProfileRepository
+    
+    init(repository: ProfileRepository) {
+        self.repository = repository
+    }
+}
+
+extension UpdateUserProfileUseCase {
+    func execute(profile: UserProfile, isPushAlarmAllowed: Bool) -> AnyPublisher<Void, WableError> {
+        return repository.updateUserProfile(profile: profile, isPushAlarmAllowed: isPushAlarmAllowed)
+            .eraseToAnyPublisher()
+    }
+}
