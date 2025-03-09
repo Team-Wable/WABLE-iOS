2eaf2697512ff92990929a3b44dd998e3eb5dc0b
youz2me <kynhun20@gachon.ac.kr>
2025-03-10 01:39:05 +0900

    [Feat] #118 - 탭 바, 네비게이션 바 및 adjust 값 적용을 위한 ConstraintMaker 익스텐션 구현

---
+++ Wable-iOS/Presentation/WableComponent/Youjin/TabBar/TabBarController.swift
@@ -0,0 +1,94 @@
+//
+//  TabBarController.swift
+//  Wable-iOS
+//
+//  Created by YOUJIM on 3/9/25.
+//
+
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    private let navigationView: NavigationView
+    
+    private let homeViewController = HomeViewController().then {
+        $0.tabBarItem.title = "홈"
+        $0.tabBarItem.image = .icHomeDefault
+    }
+    
+    private let communityViewController = CommunityViewController().then {
+        $0.tabBarItem.title = "커뮤니티"
+        $0.tabBarItem.image = .icCommunity
+    }
+    
+    private let overviewViewController = OverviewViewController().then {
+        $0.tabBarItem.title = "소식"
+        $0.tabBarItem.image = .icInfoPress
+    }
+    
+    private let viewitViewController = ViewitViewController().then {
+        $0.tabBarItem.title = "뷰잇"
+        $0.tabBarItem.image = .icViewit
+    }
+    
+    private let profileViewController = ProfileViewController().then {
+        $0.tabBarItem.title = "마이"
+        $0.tabBarItem.image = .icMyPress
+    }
+    
+    // MARK: - LifeCycle
+
+    init(navigationView: NavigationView) {
+        self.navigationView = navigationView
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    // MARK: - Setup
+
+    private func setupView() {
+        let homeNavigationController = UINavigationController(rootViewController: homeViewController)
+        let communityNavigationController = UINavigationController(rootViewController: communityViewController)
+        let overviewNavigationController = UINavigationController(rootViewController: overviewViewController)
+        let viewitNavigationController = UINavigationController(rootViewController: viewitViewController)
+        let profileNavigationController = UINavigationController(rootViewController: profileViewController)
+        
+        configureTabBar()
+        setViewControllers(
+            [
+                homeNavigationController,
+                communityNavigationController,
+                overviewNavigationController,
+                viewitNavigationController,
+                profileNavigationController
+            ],
+            animated: true
+        )
+    }
+}
+
+private extension TabBarController {
+    func configureTabBar() {
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithTransparentBackground()
+        
+        tabBar.do {
+            $0.unselectedItemTintColor = .gray400
+            $0.tintColor = .wableBlack
+            $0.backgroundColor = .wableWhite
+            $0.isTranslucent = false
+            $0.standardAppearance = tabBarAppearance
+            $0.scrollEdgeAppearance = tabBarAppearance
+        }
+    }
+}
