6c0c57ca6ea0551079ce0b54e64c577efbc3a602
youz2me <kynhun20@gachon.ac.kr>
2025-03-17 03:05:47 +0900

    [Feat] #123 - 스플래시 화면 UI 구현 및 로그인 화면 연결

---
+++ Wable-iOS/Presentation/Login/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  Wable-iOS
+//
+//  Created by YOUJIM on 3/17/25.
+//
+
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    // MARK: UIComponent
+    
+    
+}
