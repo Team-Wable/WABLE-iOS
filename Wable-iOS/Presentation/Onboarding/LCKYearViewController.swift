f1506a2debdd4c9196e5367ab96169f8e2e58e20
youz2me <kynhun20@gachon.ac.kr>
2025-03-19 03:18:01 +0900

    [Fix] #128 - 충돌 해결

--- Wable-iOS/Presentation/Onboarding/LCKYearViewController.swift
+++ Wable-iOS/Presentation/Onboarding/LCKYearViewController.swift
@@ -26,6 +26,7 @@ final class LCKYearViewController: NavigationViewController {
         setupView()
         setupConstraint()
         setupAction()
+        setupDelegate()
     }
 }
 
@@ -49,9 +50,11 @@ private extension LCKYearViewController {
     func setupAction() {
         rootView.pullDownButton.addTarget(self, action: #selector(pullDownButtonDidTap), for: .touchUpInside)
         rootView.nextButton.addTarget(self, action: #selector(nextButtonDidTap), for: .touchUpInside)
-        rootView.yearButtonList.forEach {
-            $0.addTarget(self, action: #selector(yearButtonDidTap), for: .touchUpInside)
-        }
+    }
+    
+    func setupDelegate() {
+        rootView.yearCollectionView.dataSource = self
+        rootView.yearCollectionView.delegate = self
     }
     
     // MARK: - @objc Method
@@ -62,37 +65,74 @@ private extension LCKYearViewController {
         }
         
         configuration.image = isPullDownEnabled ? .btnDropdownDown : .btnDropdownUp
-        rootView.scrollView.isHidden = isPullDownEnabled
+        rootView.yearCollectionView.isHidden = isPullDownEnabled
         rootView.pullDownButton.configuration = configuration
         
         if !isPullDownEnabled {
-            self.rootView.scrollView.alpha = 0
+            self.rootView.yearCollectionView.alpha = 0
             UIView.animate(withDuration: 0.3, animations: {
-                self.rootView.scrollView.isHidden = false
-                self.rootView.scrollView.alpha = 1
+                self.rootView.yearCollectionView.isHidden = false
+                self.rootView.yearCollectionView.alpha = 1
             })
         }
         
         isPullDownEnabled.toggle()
     }
     
-    @objc func yearButtonDidTap(_ sender: UIButton) {
-        rootView.yearButtonList.forEach {
-            guard var configuration = $0.configuration else { return }
-            let condition: Bool = ($0 == sender)
-            
-            configuration.background.backgroundColor = condition ? .purple10 : .wableWhite
-            configuration.attributedTitle = $0.titleLabel?.text?.pretendardString(with: condition ? .body1 : .body2)
-            configuration.baseForegroundColor = condition ? .purple50 : .wableBlack
-            
-            $0.configuration = configuration
-        }
-        
-        rootView.pullDownButton.configuration?.attributedTitle = sender.titleLabel?.text?
-            .pretendardString(with: .body1)
-    }
-    
     @objc func nextButtonDidTap() {
         
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension LCKYearViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let cell = collectionView.cellForItem(at: indexPath)
+//        
+//        cell
+//        
+//        rootView.yearButtonList.forEach {
+//            guard var configuration = $0.configuration else { return }
+//            let condition: Bool = ($0 == sender)
+//            
+//            configuration.background.backgroundColor = condition ? .purple10 : .wableWhite
+//            configuration.attributedTitle = $0.titleLabel?.text?.pretendardString(with: condition ? .body1 : .body2)
+//            configuration.baseForegroundColor = condition ? .purple50 : .wableBlack
+//            
+//            $0.configuration = configuration
+//        }
+//        
+//        rootView.pullDownButton.configuration?.attributedTitle = sender.titleLabel?.text?
+//            .pretendardString(with: .body1)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension LCKYearViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Calendar.current.component(.year, from: Date()) - 2012
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: UICollectionViewCell.reuseIdentifier,
+            for: indexPath
+        )
+        
+        let label = UILabel().then {
+            $0.attributedText = "\(Calendar.current.component(.year, from: Date()))".pretendardString(with: .body2)
+            $0.textColor = .wableBlack
+        }
+        
+        cell.addSubview(label)
+        
+        label.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(9)
+        }
+        
+        return cell
     }
 }
