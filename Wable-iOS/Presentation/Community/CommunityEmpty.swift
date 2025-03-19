0e78b32bc468f8a66460b1492c21a9b43088862c
JinUng41 <ingjwjw@naver.com>
2025-03-19 15:42:51 +0900

    [Setting] #132 - TabBar 및 Component 폴더 제외한 폴더링

---
+++ Wable-iOS/Presentation/Community/CommunityEmpty.swift
@@ -0,0 +1,8 @@
+//
+//  CommunityEmpty.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 3/19/25.
+//
+
+import Foundation
---
+++ Wable-iOS/Presentation/Home/HomeEmpty.swift
@@ -0,0 +1,8 @@
+//
+//  HomeEmpty.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 3/19/25.
+//
+
+import Foundation
