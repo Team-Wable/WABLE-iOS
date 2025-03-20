0c1609c45d7266304d29b0180d831ae6cf20bc97
youz2me <kynhun20@gachon.ac.kr>
2025-03-20 11:56:16 +0900

    [Feat] #126 - 3-2. 회원가입 화면(2) 응원팀 확인 구현

---
+++ Wable-iOS/Presentation/Onboarding/ViewController/LCKTeamViewController.swift
@@ -0,0 +1,110 @@
+//
+//  LCKTeamViewController.swift
+//  Wable-iOS
+//
+//  Created by YOUJIM on 3/20/25.
+//
+
+
+import UIKit
+
+final class LCKTeamViewController: NavigationViewController {
+    
+    // MARK: - Property
+    
+    private let randomTeamList: [LCKTeam] = [.t1, .gen, .bro, .drx, .dk, .kt, .ns, .bfx, .hle, .dnf].shuffled()
+    
+    // MARK: - UIComponent
+    
+    private let rootView = LCKTeamView()
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        setupConstraint()
+        setupDelegate()
+        setupAction()
+    }
+}
+
+// MARK: - Private Extension
+
+private extension LCKTeamViewController {
+    func setupView() {
+        view.addSubview(rootView)
+    }
+    
+    func setupConstraint() {
+        rootView.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom).offset(10)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
+    
+    func setupDelegate() {
+        rootView.teamCollectionView.delegate = self
+        rootView.teamCollectionView.dataSource = self
+    }
+    
+    func setupAction() {
+        rootView.skipButton.addTarget(self, action: #selector(skipButtonDidTap), for: .touchUpInside)
+        rootView.skipButton.addTarget(self, action: #selector(nextButtonDidTap), for: .touchUpInside)
+    }
+    
+    // MARK: - @objc Method
+
+    @objc func skipButtonDidTap() {
+        
+    }
+    
+    @objc func nextButtonDidTap() {
+        
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension LCKTeamViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        for cell in collectionView.visibleCells {
+            guard let cell = cell as? LCKTeamCollectionViewCell else { return }
+            
+            cell.layer.borderColor = UIColor.gray300.cgColor
+            cell.teamLabel.textColor = .gray700
+        }
+        
+        guard let cell = collectionView.cellForItem(at: indexPath) as? LCKTeamCollectionViewCell else { return }
+        
+        cell.layer.borderColor = UIColor.purple50.cgColor
+        cell.teamLabel.textColor = .wableBlack
+        
+        rootView.nextButton.updateStyle(.primary)
+        rootView.nextButton.isUserInteractionEnabled = true
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension LCKTeamViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: LCKTeamCollectionViewCell.reuseIdentifier,
+            for: indexPath
+        ) as? LCKTeamCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.teamLabel.text = randomTeamList[indexPath.row].rawValue
+        cell.teamLabel.textColor = .gray700
+        cell.teamImageView.image = UIImage(named: randomTeamList[indexPath.row].rawValue.lowercased())
+        
+        return cell
+    }
+}
