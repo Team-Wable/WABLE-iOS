4f39ba72ba947bcdc726d23e18bc8885f79d4156
JinUng41 <ingjwjw@naver.com>
2025-03-24 23:15:38 +0900

    [Add] #142 - AmplitudeManager 구현

---
+++ Wable-iOS/Presentation/UIHelper/AmplitudeManager.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 3/24/25.
+//
+
+import Foundation
+
+import AmplitudeSwift
+
+final class AmplitudeManager {
+    static let shared =  AmplitudeManager()
+    
+    private let amplitude: Amplitude
+    
+    private init() {
+        amplitude = Amplitude(configuration: Configuration(apiKey: Bundle.amplitudeAppKey))
+    }
+    
+    func trackEvent(tag: String) {
+        amplitude.track(eventType: tag)
+    }
+}
