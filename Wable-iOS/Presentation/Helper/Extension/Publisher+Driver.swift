4df4442bb8010bdf191fca4414585e5461ec605d
JinUng41 <ingjwjw@naver.com>
2025-03-30 19:05:27 +0900

    [Feat] #151 - Driver, Relay, asDriver 선언

---
+++ Wable-iOS/Presentation/Helper/Extension/Publisher+Driver.swift
@@ -0,0 +1,27 @@
+//
+//  Publisher+Driver.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 3/30/25.
+//
+
+import Combine
+import Foundation
+
+extension Publisher {
+    func asDriver(onErrorJustReturn defaultValue: Output) -> Driver<Output> {
+        return self
+            .replaceError(with: defaultValue)
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+        
+    }
+}
+
+extension Publisher where Failure == Never {
+    func asDriver() -> Driver<Output> {
+        return self
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
