c1fa0a9102c43815a12ea72b1de7b73393b25f3e
JinUng41 <ingjwjw@naver.com>
2025-03-30 19:34:29 +0900

    [Add] #151 - asDriver 개발자 문서 추가

--- Wable-iOS/Presentation/Helper/Extension/Publisher+Driver.swift
+++ Wable-iOS/Presentation/Helper/Extension/Publisher+Driver.swift
@@ -9,6 +9,16 @@ import Combine
 import Foundation
 
 extension Publisher {
+    /**
+     * Publisher를 Driver로 변환합니다.
+     *
+     * - 모든 에러를 주어진 기본값으로 대체합니다.
+     * - 메인 스레드에서 결과를 전달합니다.
+     * - AnyPublisher로 타입을 지웁니다.
+     *
+     * - Parameter defaultValue: 에러 발생 시 대체할 기본값
+     * - Returns: 메인 스레드에서 동작하고 에러가 없는 Driver<Output>
+     */
     func asDriver(onErrorJustReturn defaultValue: Output) -> Driver<Output> {
         return self
             .replaceError(with: defaultValue)
@@ -18,6 +28,14 @@ extension Publisher {
 }
 
 extension Publisher where Failure == Never {
+    /**
+     * 에러가 없는 Publisher를 Driver로 변환합니다.
+     *
+     * - 메인 스레드에서 결과를 전달합니다.
+     * - AnyPublisher로 타입을 지웁니다.
+     *
+     * - Returns: 메인 스레드에서 동작하는 Driver<Output>
+     */
     func asDriver() -> Driver<Output> {
         return self
             .receive(on: DispatchQueue.main)
