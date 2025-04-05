37ace87866a4495244ca2b1073ff252578c23639
JinUng41 <ingjwjw@naver.com>
2025-04-06 02:38:43 +0900

    [Fix] #159 - `setPretendard` 삭제 및 대체

--- Wable-iOS/Presentation/Helper/Extension/UITextField+.swift
+++ Wable-iOS/Presentation/Helper/Extension/UITextField+.swift
@@ -9,33 +9,6 @@ import UIKit
 
 extension UITextField {
     
-    // MARK: - setPretendard
-
-    /// 주어진 `Pretendard` 스타일을 `UITextField`의 기본 텍스트 속성에 적용합니다.
-    ///
-    /// - Parameter style: 적용할 `UIFont.Pretendard` 스타일
-    ///
-    /// 사용 예시:
-    /// ```swift
-    /// let textField = UITextField()
-    /// textField.setPretendard(with: .body1)
-    /// textField.attributedPlaceholder = "Enter your text".pretendardString(with: .body1)
-    /// ```
-    func setPretendard(with style: UIFont.Pretendard) {
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.minimumLineHeight = style.lineHeight
-        paragraphStyle.maximumLineHeight = style.lineHeight
-        
-        let attributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.pretendard(style),
-            .kern: style.kerning,
-            .paragraphStyle: paragraphStyle,
-            .baselineOffset: style.baselineOffset
-        ]
-        
-        self.defaultTextAttributes = attributes
-    }
-    
     // MARK: - Pretendard style Initializer
     
     /// Pretendard 폰트 스타일을 적용한 UITextField를 생성하는 편의 생성자입니다.
