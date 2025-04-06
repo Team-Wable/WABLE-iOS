9acad53ac4cc890ed882af7cd59bd102afe84ff9
youz2me <kynhun20@gachon.ac.kr>
2025-04-06 23:02:37 +0900

    [Feat] #139 - 세부 화면 UI 구현

---
+++ Wable-iOS/Presentation/Home/ViewModel/HomeDetailViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  HomeDetailViewModel.swift
+//  Wable-iOS
+//
+//  Created by YOUJIM on 4/5/25.
+//
+
+
+import Combine
+
+final class HomeDetailViewModel {
+    
+}
+
+extension HomeDetailViewModel: ViewModelType {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input, cancelBag: CancelBag) -> Output {
+        <#code#>
+    }
+}
