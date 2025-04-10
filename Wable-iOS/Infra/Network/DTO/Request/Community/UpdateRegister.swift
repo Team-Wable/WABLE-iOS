4caf1510d8bb6cd2b0592d61c1a3511635bd9286
JinUng41 <ingjwjw@naver.com>
2025-04-10 21:39:16 +0900

    [Refactor] #161 - PreRegister를 Register로 변경
    - 사전 신청이라는 의미에서 PreRegister가 적합할 수 있으나, 너무 네이밍이 길어진다고 판단하여 좀 더 간결한 형태를 유지하고자 변경함.

--- Wable-iOS/Infra/Network/DTO/Request/Community/UpdateRegister.swift
+++ Wable-iOS/Infra/Network/DTO/Request/Community/UpdateRegister.swift
@@ -1,5 +1,5 @@
 //
-//  UpdatePreRegister.swift
+//  UpdateRegister.swift
 //  Wable-iOS
 //
 //  Created by YOUJIM on 2/20/25.
@@ -11,7 +11,7 @@ import Foundation
 // MARK: - 커뮤니티 사전 참여
 
 extension DTO.Request {
-    struct UpdatePreRegister: Encodable {
+    struct UpdateRegister: Encodable {
         let communityName: String
     }
 }
