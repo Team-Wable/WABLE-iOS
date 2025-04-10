02b40765af94330907df6a78b54acd64f7e902bc
JinUng41 <ingjwjw@naver.com>
2025-04-10 21:56:20 +0900

    [Feat] #161 - 유즈케이스, 모델 구현

--- Wable-iOS/Domain/UseCase/Community/CommunityUseCase.swift
+++ Wable-iOS/Domain/UseCase/Community/CommunityUseCase.swift
@@ -13,3 +13,23 @@ protocol CommunityUseCase {
     func fetchCommunityList() -> AnyPublisher<[Community], WableError>
     func register(for communityTeam: LCKTeam) -> AnyPublisher<Double, WableError>
 }
+
+final class CommunityUseCaseImpl: CommunityUseCase {
+    private let repository: CommunityRepository
+    
+    init(repository: CommunityRepository) {
+        self.repository = repository
+    }
+    
+    func isUserRegistered() -> AnyPublisher<CommunityRegistrationStatus, WableError> {
+        return repository.isUserRegistered()
+    }
+    
+    func fetchCommunityList() -> AnyPublisher<[Community], WableError> {
+        return repository.fetchCommunityList()
+    }
+    
+    func register(for communityTeam: LCKTeam) -> AnyPublisher<Double, WableError> {
+        return repository.updateRegister(communityName: communityTeam.rawValue)
+    }
+}
