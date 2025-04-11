88b9d1e22c206c47bec7e94393e3fcbb2ce50cf8
JinUng41 <ingjwjw@naver.com>
2025-04-12 02:10:23 +0900

    [Add] #158 - 뷰잇 카드 뷰를 위한 버튼 구현

---
+++ Wable-iOS/Presentation/Viewit/View/ViewitViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewitViewController.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 4/12/25.
+//
+
+
+import UIKit
+
+final class ViewitViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .wableWhite
+    }
+}
