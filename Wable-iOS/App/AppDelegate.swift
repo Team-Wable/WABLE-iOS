0f1c2e10e9a1167aaf28891d7c54e60eaf6dcdcf
youz2me <kynhun20@gachon.ac.kr>
2025-04-30 03:40:35 +0900

    [Feat] #177 - FCM 기능 구현

--- Wable-iOS/App/AppDelegate.swift
+++ Wable-iOS/App/AppDelegate.swift
@@ -11,11 +11,25 @@ import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: Property
+    // TODO: Repository를 UseCase로 변경
+    
+    let userBadgeUseCase = UpdateUserBadgeUseCase(repository: AccountRepositoryImpl())
+    let userSessionRepository = UserSessionRepositoryImpl(userDefaults: UserDefaultsStorage(jsonEncoder: JSONEncoder(), jsonDecoder: JSONDecoder()))
+    let profileRepository = ProfileRepositoryImpl()
+    let cancelBag = CancelBag()
+    
+    var activeID = -1
+    
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         injectDependency()
+        fetchActiveID()
+        configureFirebase(application: application)
+        requestNotificationPermission()
         
         KakaoSDK.initSDK(appKey: Bundle.kakaoAppKey)
         
@@ -31,6 +45,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     ) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+}
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+// MARK: - Helper Method
+
+private extension AppDelegate {
+    func fetchActiveID() {
+        guard let id = self.userSessionRepository.fetchActiveUserID()
+        else {
+            WableLogger.log("활성 세션의 아이디를 불러오지 못했습니다.", for: .error)
+            
+            return
+        }
+        
+        activeID = id
+    }
+    
+    func requestNotificationPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { [weak self] granted, error in
+            guard let self = self,
+                  let activeSession = userSessionRepository.fetchActiveUserSession() else { return }
+            
+            self.userSessionRepository.updateUserSession(
+                UserSession(
+                    id: activeSession.id,
+                    nickname: activeSession.nickname,
+                    profileURL: activeSession.profileURL,
+                    isPushAlarmAllowed: granted,
+                    isAdmin: activeSession.isAdmin,
+                    isAutoLoginEnabled: activeSession.isAutoLoginEnabled,
+                    notificationBadgeCount: activeSession.notificationBadgeCount
+                )
+            )
+        }
+    }
 }
