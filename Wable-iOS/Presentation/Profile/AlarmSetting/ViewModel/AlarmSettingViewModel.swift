8d7deff45228f038c2b643437ca2d6142c62cc09
JinUng41 <ingjwjw@naver.com>
2025-05-13 03:20:33 +0900

    [Feat] #182 - 알람 설정 화면 및 기능 구현

---
+++ Wable-iOS/Presentation/Profile/AlarmSetting/ViewModel/AlarmSettingViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  AlarmSettingViewModel.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 5/13/25.
+//
+
+import Combine
+import Foundation
+import UserNotifications
+
+final class AlarmSettingViewModel {
+    @Published private(set) var isAuthorized: Bool = false
+    
+    func checkAlarmAuthorization() {
+        Task {
+            let settings = await UNUserNotificationCenter.current().notificationSettings()
+            isAuthorized = settings.authorizationStatus == .authorized
+        }
+    }
+}
