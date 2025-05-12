4e2442241f791a98082b69c487ef751a81f1f047
JinUng41 <ingjwjw@naver.com>
2025-05-12 19:34:52 +0900

    [Refactor] #158 - 유즈케이스 분리

---
+++ Wable-iOS/Domain/UseCase/Viewit/ReportViewitUseCase.swift
@@ -0,0 +1,34 @@
+//
+//  ReportViewitUseCase.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 5/12/25.
+//
+
+import Combine
+import Foundation
+
+protocol ReportViewitUseCase {
+    func report(viewit: Viewit) -> AnyPublisher<Viewit?, WableError>
+    func ban(viewit: Viewit) -> AnyPublisher<Viewit?, WableError>
+}
+
+final class ReportViewitUseCaseImpl: ReportViewitUseCase {
+    @Injected(config: .debug) private var repository: ReportRepository
+    
+    func report(viewit: Viewit) -> AnyPublisher<Viewit?, WableError> {
+        return repository.createReport(nickname: viewit.userNickname, text: viewit.text)
+            .map { viewit }
+            .eraseToAnyPublisher()
+    }
+    
+    func ban(viewit: Viewit) -> AnyPublisher<Viewit?, WableError> {
+        return repository.createBan(memberID: viewit.userID, triggerType: .viewit, triggerID: viewit.id)
+            .map {
+                var bannedViewit = viewit
+                bannedViewit.status = .blind
+                return bannedViewit
+            }
+            .eraseToAnyPublisher()
+    }
+}
--- Wable-iOS/Domain/UseCase/Viewit/ViewitUseCase.swift
+++ Wable-iOS/Domain/UseCase/Viewit/ViewitUseCase.swift
@@ -10,63 +10,19 @@ import Foundation
 
 protocol ViewitUseCase {
     func fetchViewitList(last viewitID: Int) -> AnyPublisher<[Viewit], WableError>
-    func like(viewit: Viewit) -> AnyPublisher<Viewit?, WableError>
-    func unlinke(viewit: Viewit) -> AnyPublisher<Viewit?, WableError>
     func delete(viewit: Viewit) -> AnyPublisher<Viewit?, WableError>
-    func report(viewit: Viewit) -> AnyPublisher<Viewit?, WableError>
-    func ban(viewit: Viewit) -> AnyPublisher<Viewit?, WableError>
 }
 
 final class ViewitUseCaseImpl: ViewitUseCase {
-    
-    // TODO: 기능 구현 후, 객체 교체
-    
     @Injected(config: .debug) private var viewitRepository: ViewitRepository
-    @Injected(config: .debug) private var reportRepository: ReportRepository
     
     func fetchViewitList(last viewitID: Int) -> AnyPublisher<[Viewit], WableError> {
         return viewitRepository.fetchViewitList(cursor: viewitID)
     }
     
-    func like(viewit: Viewit) -> AnyPublisher<Viewit?, WableError> {
-        return viewitRepository.postViewitLiked(viewitID: viewit.id)
-            .map {
-                var likedViewit = viewit
-                likedViewit.like.like()
-                return likedViewit
-            }
-            .eraseToAnyPublisher()
-    }
-    
-    func unlinke(viewit: Viewit) -> AnyPublisher<Viewit?, WableError> {
-        return viewitRepository.deleteViewitLiked(viewitID: viewit.id)
-            .map {
-                var unlikedViewit = viewit
-                unlikedViewit.like.unlike()
-                return unlikedViewit
-            }
-            .eraseToAnyPublisher()
-    }
-    
     func delete(viewit: Viewit) -> AnyPublisher<Viewit?, WableError> {
         return viewitRepository.deleteViewit(viewitID: viewit.id)
             .map { viewit }
-            .eraseToAnyPublisher()
-    }
-    
-    func report(viewit: Viewit) -> AnyPublisher<Viewit?, WableError> {
-        return reportRepository.createReport(nickname: viewit.userNickname, text: viewit.text)
-            .map { viewit }
-            .eraseToAnyPublisher()
-    }
-    
-    func ban(viewit: Viewit) -> AnyPublisher<Viewit?, WableError> {
-        return reportRepository.createBan(memberID: viewit.userID, triggerType: .viewit, triggerID: viewit.id)
-            .map {
-                var bannedViewit = viewit
-                bannedViewit.status = .blind
-                return bannedViewit
-            }
             .eraseToAnyPublisher()
     }
 }
