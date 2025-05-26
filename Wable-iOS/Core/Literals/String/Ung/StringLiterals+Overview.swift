ef93756113305127f3ffc4fd4991019f61e55927
JinUng41 <ingjwjw@naver.com>
2025-05-27 01:27:15 +0900

    [Refactor] #203 - StringLiterals+Overview 추가 및 적용

---
+++ Wable-iOS/Core/Literals/String/Ung/StringLiterals+Overview.swift
@@ -0,0 +1,18 @@
+//
+//  StringLiterals+Overview.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 5/27/25.
+//
+
+import Foundation
+
+extension StringLiterals {
+    
+    // MARK: - Overview
+
+    enum Overview {
+        static let feedbackButtonLongTitle = "더 알고 싶은 정보가 있다면? 의견 남기러 가기"
+        static let feedbackButtonShortTitle = "와블에 대한 의견 남기러 가기"
+    }
+}
