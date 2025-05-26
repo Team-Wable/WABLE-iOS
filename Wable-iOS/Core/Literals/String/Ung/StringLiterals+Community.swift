514f41d90d45da06047bd521656c41bd4be6ddcb
JinUng41 <ingjwjw@naver.com>
2025-05-27 01:18:23 +0900

    [Refactor] #203 - StringLiterals+Community 추가 및 적용

---
+++ Wable-iOS/Core/Literals/String/Ung/StringLiterals+Community.swift
@@ -0,0 +1,29 @@
+//
+//  StringLiterals+Community.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 5/27/25.
+//
+
+import Foundation
+
+extension StringLiterals {
+    
+    // MARK: - Community
+    
+    enum Community {
+        static let description = "팀별 커뮤니티 공간을 준비중이에요. 팀별로 일정 이상의 팬이 모여야 팀별 공간이 열립니다.\n계정 1개당 1개의 팀별 공간에만 참여 가능해요!"
+        static let askButtonTitle = "더 추가하고 싶은 게시판이 있다면? 요청하기"
+        
+        static let registerSheetTitle = "사전 신청하시겠어요?"
+        static let registerSheetMessage = "1개의 팀별 공간에만 참여가 가능하다는\n점 꼭 기억해주세요!"
+        
+        static let completeSheetTitle = "신청을 완료했어요"
+        
+        static let alarmSheetTitle = "푸시 알림 안내"
+        static let alarmSheetMessage = "푸시 알림을 켜두면 팀별 커뮤니티가\n오픈됐을 때 알림으로 안내드려요!"
+        
+        static let inviteSheetTitle = "링크가 복사되었어요"
+        static let inviteSheetMessage = "복사된 링크를 널리널리 퍼뜨려\n함께 응원할 팬을 더 많이 데려와주세요!"
+    }
+}
