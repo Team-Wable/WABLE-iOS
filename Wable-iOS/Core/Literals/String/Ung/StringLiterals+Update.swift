c7489e568ffd8305ef7352833b11f9538f531baf
JinUng41 <ingjwjw@naver.com>
2025-05-27 01:52:31 +0900

    [Refactor] #203 - StringLiterals+Update 추가 및 적용

---
+++ Wable-iOS/Core/Literals/String/Ung/StringLiterals+Update.swift
@@ -0,0 +1,18 @@
+//
+//  StringLiterals+Update.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 5/27/25.
+//
+
+import Foundation
+
+extension StringLiterals {
+    
+    // MARK: - Update
+
+    enum Update {
+        static let title = "업데이트"
+        static let message = "와블이 새롭게 업그레이드 되었어요.\n업데이트를 통해 최신 버전으로 즐겨보세요."
+    }
+}
