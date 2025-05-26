a73705f76d638554409fe5a36ae03249a34ab82e
youz2me <kynhun20@gachon.ac.kr>
2025-05-27 03:22:42 +0900

    [Refactor] #203 - Login, Onboarding, Profile, Write, Report, Ghost, Exit, Empty, Detail, Delete Literal 작성

---
+++ Wable-iOS/Core/Literals/String/StringLiterals+Report.swift
@@ -0,0 +1,20 @@
+//
+//  StringLiterals+Report.swift
+//  Wable-iOS
+//
+//  Created by YOUJIM on 5/27/25.
+//
+
+
+import Foundation
+
+extension StringLiterals {
+    
+    // MARK: - Report
+
+    enum Report {
+        static let sheetTitle = "신고하시겠어요?"
+        static let sheetMessage = "해당 유저 혹은 게시글을 신고하시려면 신고하기 버튼을 눌러주세요"
+        static let completeToast = "답글을 남겼어요"
+    }
+}
---
+++ Wable-iOS/Core/Literals/String/StringLiterals+Write.swift
@@ -0,0 +1,19 @@
+//
+//  StringLiterals+Write.swift
+//  Wable-iOS
+//
+//  Created by YOUJIM on 5/27/25.
+//
+
+
+import Foundation
+
+extension StringLiterals {
+    
+    // MARK: - Write
+
+    enum Write {
+        static let sheetTitle = "자유롭게 이야기해요"
+        static let sheetMessage = "지금 머릿속에 떠오른 생각들을 남겨보세요\n본문은 생략이 가능해요"
+    }
+}
