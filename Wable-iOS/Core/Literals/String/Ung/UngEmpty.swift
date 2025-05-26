8efcc9c3cb26c3517fd3028a6cebb29969395f81
JinUng41 <ingjwjw@naver.com>
2025-05-27 00:50:41 +0900

    [Add] #203 - StringLiterals 선언 및 작업 폴더 분류

---
+++ Wable-iOS/Core/Literals/String/Ung/UngEmpty.swift
@@ -0,0 +1,8 @@
+//
+//  UngEmpty.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 5/27/25.
+//
+
+import Foundation
---
+++ Wable-iOS/Core/Literals/String/Youjin/YoujinEmpty.swift
@@ -0,0 +1,8 @@
+//
+//  YoujinEmpty.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 5/27/25.
+//
+
+import Foundation
