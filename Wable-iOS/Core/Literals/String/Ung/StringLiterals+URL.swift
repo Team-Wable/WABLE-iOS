776efe404f7eacde9906622586d0d89d087c7059
JinUng41 <ingjwjw@naver.com>
2025-05-27 01:58:30 +0900

    [Refactor] #203 - StringLiterals+URL itunes, appstore 추가 및 적용

--- Wable-iOS/Core/Literals/String/Ung/StringLiterals+URL.swift
+++ Wable-iOS/Core/Literals/String/Ung/StringLiterals+URL.swift
@@ -16,5 +16,7 @@ extension StringLiterals {
         static let privacyPolicy = "https://joyous-ghost-8c7.notion.site/fff08b005ea18052ae0bf9d056c2e830"
         static let feedbackForm = "https://docs.google.com/forms/d/e/1FAIpQLSf3JlBkVRPaPFSreQHaEv-u5pqZWZzk7Y4Qll9lRP0htBZs-Q/viewform?pli=1"
         static let littly = "https://litt.ly/wable"
+        static let itunes = "https://itunes.apple.com/kr/lookup?bundleId=com.wable.Wable-iOS&country=kr"
+        static let appStore = "itms-apps://itunes.apple.com/app/apple-store/id6670352454"
     }
 }
