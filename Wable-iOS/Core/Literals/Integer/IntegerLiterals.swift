006d046291742ffb2b83e30be96673c40904e75c
JinUng41 <ingjwjw@naver.com>
2025-05-27 00:49:00 +0900

    [Add] #203 - IntegerLiterals 추가

---
+++ Wable-iOS/Core/Literals/Integer/IntegerLiterals.swift
@@ -0,0 +1,14 @@
+//
+//  IntegerLiterals.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 5/27/25.
+//
+
+import Foundation
+
+enum IntegerLiterals {
+    static let initialCursor = -1
+    static let defaultCountPerPage = 15
+    static let commentCountPerPage = 10
+}
