fc636cc7f810f7d844062cb5688295e6fda8049e
JinUng41 <ingjwjw@naver.com>
2025-06-23 00:52:58 +0900

    [Feat] #236 - WableTextSheetShowable 구현

---
+++ Wable-iOS/Presentation/Helper/WableTextSheetShowable.swift
@@ -0,0 +1,48 @@
+//
+//  WableTextSheetShowable.swift
+//  Wable-iOS
+//
+//  Created by 김진웅 on 6/23/25.
+//
+
+import UIKit
+
+// MARK: - WableTextSheetShowable
+
+protocol WableTextSheetShowable {
+    func showWableTextSheet(title: String, placeholder: String, actions: WableTextSheetAction...)
+}
+
+extension WableTextSheetShowable where Self: UIViewController {
+    func showWableTextSheet(title: String, placeholder: String, actions: WableTextSheetAction...) {
+        let wableTextSheet = WableTextSheetViewController(title: title, placeholder: placeholder)
+        actions.forEach { wableTextSheet.addAction($0) }
+        present(wableTextSheet, animated: true)
+    }
+    
+    func showGhostSheet(_ handler: @escaping (String?) -> Void) {
+        let wableTextSheet = WableTextSheetViewController(
+            title: StringLiterals.Ghost.sheetTitle,
+            placeholder: StringLiterals.Ghost.sheetPlaceholder
+        )
+        let cancel = WableTextSheetAction(title: "고민할게요", style: .gray)
+        let confirm = WableTextSheetAction(title: "투명도 내리기", style: .primary, handler: handler)
+        wableTextSheet.addActions(cancel, confirm)
+        present(wableTextSheet, animated: true)
+    }
+    
+    func showReportSheet(_ handler: @escaping (String?) -> Void) {
+        let wableTextSheet = WableTextSheetViewController(
+            title: StringLiterals.Report.sheetTitle,
+            placeholder: StringLiterals.Report.sheetPlaceholder
+        )
+        let cancel = WableTextSheetAction(title: "고민할게요", style: .gray)
+        let confirm = WableTextSheetAction(title: "신고하기", style: .primary, handler: handler)
+        wableTextSheet.addActions(cancel, confirm)
+        present(wableTextSheet, animated: true)
+    }
+}
+
+// MARK: - UIViewController Extension
+
+extension UIViewController: WableTextSheetShowable {}
