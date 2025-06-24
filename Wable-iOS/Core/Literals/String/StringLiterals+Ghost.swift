795e0fd7ecf375f2ac348a4b42db55a9abc1e2b0
JinUng41 <ingjwjw@naver.com>
2025-06-24 15:38:20 +0900

    [Add] #238 - 툴팁 문구 추가

--- Wable-iOS/Core/Literals/String/StringLiterals+Ghost.swift
+++ Wable-iOS/Core/Literals/String/StringLiterals+Ghost.swift
@@ -15,5 +15,6 @@ extension StringLiterals {
     enum Ghost {
         static let sheetTitle = "와블의 온화한 문화를 해치는\n누군가를 발견하신 건가요?"
         static let completeToast = "덕분에 와블이 더 온화해지고 있어요!"
+        static let tooltip = "투명도란? 설명어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구"
     }
 }
