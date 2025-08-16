044a17f45a7faeec1928ce47593f3512c89d6a07
youz2me <kynhun20@gachon.ac.kr>
2025-08-16 17:00:00 +0900

    [Refactor] #259 사진 권한 익스텐션 추가

--- Wable-iOS/Presentation/Helper/Extension/UIViewController+PHPicker.swift
+++ Wable-iOS/Presentation/Helper/Extension/UIViewController+PHPicker.swift
@@ -11,6 +11,23 @@ import Photos
 import PhotosUI
 
 extension UIViewController {
+    func setPhotoPermission(delegate: PHPickerViewControllerDelegate) {
+        switch PHPhotoLibrary.authorizationStatus(for: .addOnly) {
+        case .denied, .restricted:
+            presentSettings()
+        case .authorized, .limited:
+            presentPhotoPicker(delegate: delegate)
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization(for: .addOnly) { status in
+                DispatchQueue.main.async {
+                    if status == .authorized { self.presentPhotoPicker(delegate: delegate) }
+                }
+            }
+        default:
+            break
+        }
+    }
+    
     func presentPhotoPicker(delegate: PHPickerViewControllerDelegate) {
         var configuration = PHPickerConfiguration()
         configuration.filter = .images
@@ -24,16 +41,10 @@ extension UIViewController {
     
     func presentSettings() {
         guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
-        let alert = UIAlertController(
-            title: "설정",
-            message: StringLiterals.Empty.photoPermission,
-            preferredStyle: .alert
-        )
-        
+        let message = StringLiterals.Empty.photoPermission
+        let alert = UIAlertController(title: "설정", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "닫기", style: .default))
-        alert.addAction(UIAlertAction(title: "권한 설정하기", style: .default) { _ in
-            UIApplication.shared.open(url)
-        })
+        alert.addAction(UIAlertAction(title: "권한 설정하기", style: .default) { _ in UIApplication.shared.open(url) })
         
         present(alert, animated: true, completion: nil)
     }
