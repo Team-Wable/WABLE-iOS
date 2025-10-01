4d7c97e16290fba5cf47bf45943698b94de8d07f
youz2me <kynhun20@gachon.ac.kr>
2025-10-01 11:36:04 +0900

    [Refactor] #280 - Coordinator 메모리 누수 방지 및 완료 패턴 개선
    - Coordinator 프로토콜에 onFinish 콜백 추가
    - childDidFinish 메서드 추가하여 자식 Coordinator 정리
    - OnboardingCoordinator 완료 시 childCoordinators에서 제거
    - LoginCoordinator에 onFinish 프로퍼티 추가
    - 일관된 Coordinator 생명주기 관리 패턴 적용

--- Wable-iOS/Core/Coordinator/Coordinator.swift
+++ Wable-iOS/Core/Coordinator/Coordinator.swift
@@ -10,6 +10,19 @@ import UIKit
 protocol Coordinator: AnyObject {
     var childCoordinators: [Coordinator] { get set }
     var navigationController: UINavigationController { get set }
+    var onFinish: (() -> Void)? { get set }
 
     func start()
+    func childDidFinish(_ child: Coordinator?)
+}
+
+extension Coordinator {
+    func childDidFinish(_ child: Coordinator?) {
+        for (index, coordinator) in childCoordinators.enumerated() {
+            if coordinator === child {
+                childCoordinators.remove(at: index)
+                break
+            }
+        }
+    }
 }
