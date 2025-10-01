1b6663f30e700878638f32cf2c66c0c41c7a86b9
youz2me <kynhun20@gachon.ac.kr>
2025-10-01 10:47:49 +0900

    [Refactor] #280 - 온보딩/프로필 공통 타입 분리

---
+++ Wable-iOS/Presentation/Enum/NicknameValidationResult.swift
@@ -0,0 +1,14 @@
+//
+//  NicknameValidationResult.swift
+//  Wable-iOS
+//
+//  Created by YOUJIM on 10/1/25.
+//
+
+import Foundation
+
+enum NicknameValidationResult {
+    case empty
+    case valid
+    case invalidFormat
+}
