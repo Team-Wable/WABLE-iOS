e6a23a59d7c26d8da4e3e0cec629a8c563b631b2
youz2me <kynhun20@gachon.ac.kr>
2025-10-25 01:00:07 +0900

    [Feat] #294 - 퀴즈 도메인 엔티티 및 Repository 구현

---
+++ Wable-iOS/Data/Mapper/QuizMapper.swift
@@ -0,0 +1,21 @@
+//
+//  QuizMapper.swift
+//  Wable-iOS
+//
+//  Created by Youjin Lee on 10/25/25.
+//
+
+import Foundation
+
+enum QuizMapper { }
+
+extension QuizMapper {
+    static func toDomain(_ response: DTO.Response.FetchQuiz) -> Quiz {
+        return Quiz(
+            id: response.id,
+            imageURL: response.imageURL,
+            text: response.text,
+            answer: response.answer
+        )
+    }
+}
