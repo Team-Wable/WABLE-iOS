ca054ea57191dfd5c61eae3f589e120600b00d47
youz2me <kynhun20@gachon.ac.kr>
2025-10-26 04:29:00 +0900

    [Feat] #294 - 퀴즈 관련 StringLiterals 추가
    - Quiz enum에 퀴즈 화면 텍스트 상수 정의
    - 정답/오답 메시지, 로딩 에러 메시지 등 추가

---
+++ Wable-iOS/Core/Literals/String/StringLiterals+Quiz.swift
@@ -0,0 +1,26 @@
+//
+//  StringLiterals+Quiz.swift
+//  Wable-iOS
+//
+//  Created by Youjin Lee on 10/26/25.
+//
+
+import Foundation
+
+extension StringLiterals {
+    
+    // MARK: - Quiz
+
+    enum Quiz {
+        static let correctTitle = "걸어다니는 LCK 백과사전"
+        static let correctDescription = "정답을 맞추셨어요. 대단해요!"
+        static let incorrectTitle = "LCK의 평범한 시청자!"
+        static let incorrectDescription = "정답은 틀렸지만, 좋은 시도였어요!\n다음 기회를 노려봐요"
+        static let nextQuizTitle = "다음 퀴즈 참여까지\n남은 시간"
+        static let feedbackTitle = "와블에 대한 새로운 의견이 있다면? →"
+        static let loadingErrorTitle = "퀴즈 로딩 중 오류가 발생했어요"
+        static let rewardErrorTitle = "리워드 처리 중 오류가 발생했어요"
+        static let loadingErrorMessage = "다시 시도해주세요."
+        
+    }
+}
