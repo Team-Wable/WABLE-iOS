ff44a287c380ab269cf5cab02485c65c3c4a1adf
youz2me <kynhun20@gachon.ac.kr>
2025-10-26 00:34:05 +0900

    feat: #294 - 퀴즈 뷰모델 Input-Output 패턴 구현

--- Wable-iOS/Presentation/Quiz/ViewModel/QuizResultViewModel.swift
+++ Wable-iOS/Presentation/Quiz/ViewModel/QuizResultViewModel.swift
@@ -9,17 +9,61 @@
 import Combine
 import Foundation
 
-public final class QuizResultViewModel {
-    
+final class QuizResultViewModel {
+
     // MARK: Property
-    
-    private let answer: Bool
-    private let totalTime: Int
-    
-    // MARK: - LifeCycle
-    
-    init(answer: Bool, totalTime: Int) {
-        self.answer = answer
-        self.totalTime = totalTime
+
+    private let updateQuizResultSubject = PassthroughSubject<Int, Never>()
+    private let errorSubject = PassthroughSubject<WableError, Never>()
+
+    @Injected private var quizRepository: QuizRepository
+}
+
+extension QuizResultViewModel: ViewModelType {
+    struct Input {
+        let rewardButtonDidTap: AnyPublisher<(quizId: Int, answer: Bool, totalTime: Int), Never>
+    }
+
+    struct Output {
+        let updateQuizResult: AnyPublisher<Int, Never>
+        let error: AnyPublisher<WableError, Never>
+    }
+
+    func transform(input: Input, cancelBag: CancelBag) -> Output {
+        input.rewardButtonDidTap
+            .withUnretained(self)
+            .flatMap { owner, data -> AnyPublisher<Int, Never> in
+                return owner.updateQuizAnswer(
+                    id: data.quizId,
+                    answer: data.answer,
+                    totalTime: data.totalTime
+                )
+            }
+            .sink(receiveValue: { [weak self] xpValue in
+                self?.updateQuizResultSubject.send(xpValue)
+            })
+            .store(in: cancelBag)
+
+        return Output(
+            updateQuizResult: updateQuizResultSubject.eraseToAnyPublisher(),
+            error: errorSubject.eraseToAnyPublisher()
+        )
+    }
+}
+
+// MARK: - Helper Method
+
+private extension QuizResultViewModel {
+    func updateQuizAnswer(id: Int, answer: Bool, totalTime: Int) -> AnyPublisher<Int, Never> {
+        return quizRepository.updateQuizAnswer(
+            id: id,
+            answer: answer,
+            totalTime: totalTime
+        )
+        .catch { [weak self] error -> AnyPublisher<Int, Never> in
+            self?.errorSubject.send(error)
+            return Empty<Int, Never>().eraseToAnyPublisher()
+        }
+        .eraseToAnyPublisher()
     }
 }
