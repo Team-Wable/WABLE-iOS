443bd1393d7dbd435adcbef48b293ec27aef7476
youz2me <kynhun20@gachon.ac.kr>
2025-10-26 01:53:44 +0900

    [Chore] #294 - 불필요한 코드 정리 및 프로젝트 파일 업데이트
    - QuizCoordinator 사용하지 않는 메서드 주석 처리
    - Xcode 프로젝트 파일 업데이트

--- Wable-iOS/Presentation/Quiz/Coordinator/QuizCoordinator.swift
+++ Wable-iOS/Presentation/Quiz/Coordinator/QuizCoordinator.swift
@@ -31,6 +31,7 @@ public extension QuizCoordinator {
 <<<<<<< HEAD:Wable-iOS/Presentation/Quiz/Coordinator/QuizCoordinator.swift
 =======
     
+<<<<<<< HEAD:Wable-iOS/Presentation/Quiz/Coordinator/QuizCoordinator.swift
     func showResultView(isCorrect: Bool) {
         let viewController = QuizResultViewController(
             viewModel: QuizResultViewModel(),
@@ -41,4 +42,15 @@ public extension QuizCoordinator {
         navigationController.present(viewController, animated: true)
     }
 >>>>>>> fabc822 (feat: #294 - 퀴즈 화면 UI 구현 및 네비게이션 설정):Wable-iOS/Presentation/Quiz/QuizCoordinator.swift
+=======
+//    func showResultView(isCorrect: Bool) {
+//        let viewController = QuizResultViewController(
+//            viewModel: QuizResultViewModel(),
+//            answer: isCorrect,
+//            totalTime: 0
+//        )
+//
+//        navigationController.present(viewController, animated: true)
+//    }
+>>>>>>> 4d5787a ([Chore] #294 - 불필요한 코드 정리 및 프로젝트 파일 업데이트):Wable-iOS/Presentation/Quiz/QuizCoordinator.swift
 }
